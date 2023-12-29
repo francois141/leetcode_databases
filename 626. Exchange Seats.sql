785582bbb6f3c81ab04d6334d67ec5d7544069f0
francois141 <47925812+francois141@users.noreply.github.com>
2023-12-29 14:58:07 +0100

    Create 626. Exchange Seats.sql

---
+++ 626. Exchange Seats.sql
@@ -0,0 +1,6 @@
+SELECT 
+CASE
+    WHEN (id % 2) = 1 AND (id+1) IN (SELECT id FROM Seat) THEN id+1
+    WHEN (id % 2) = 1 AND (id+1) NOT IN (SELECT id FROM Seat) THEN id
+    ELSE id-1
+END AS id, student FROM Seat ORDER BY id;
